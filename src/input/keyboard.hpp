ccd0f77aead15f628a2c0c06237a5463ba007863
Danilo <borges_santos89@hotmail.com>
2023-10-26 10:18:45 -0300

    Implementa uso de slots em KeyboardState

--- src/input/keyboard.hpp
+++ src/input/keyboard.hpp
@@ -11,66 +11,36 @@ namespace dxna::input {
 
 		//Obtém TRUE caso a tecla informada esteja pressionada.
 		constexpr bool IsKeyDown(Keys const& key) const {
-			if (index == 0)
-				return false;
+			if (index < 0)
+				return false;	
 
-			for (size_t i = 0; i < MAX_KEYS; ++i) {
-				if (downKeys[i] == (int)key)
-					return true;				
-				else if (i == -1)
-					return false;
-			}
-
-			return false;
+			return checkSlots((int)key);
 		}
 
 		//Obtém TRUE caso a tecla informada esteja pressionada.
-		bool constexpr IsKeyDown(size_t key) const {
-			if (index == 0 || key > 255)
+		bool constexpr IsKeyDown(size_t vkkey) const {
+			if (index < 0)
 				return false;
 
-			for (size_t i = 0; i < MAX_KEYS; ++i) {
-				if (downKeys[i] == key)
-					return true;
-				else if (i == -1)
-					return false;
-
-			}
+			return checkSlots(static_cast<int>(vkkey));
 
 			return false;
 		}
 
 		//Obtém TRUE caso a tecla informada esteja liberada.
 		constexpr bool IsKeyUp(Keys const& key) const {		
-			if (index == 0)
+			if (index < 0)
 				return true;
 
-			for (size_t i = 0; i < MAX_KEYS; ++i) {
-				if (downKeys[i] == (int)key)
-					return false;
-				else if (i == -1)
-					return true;
-			}
-
-			return true;
+			return !checkSlots((int)key);
 		}
 
 		//Obtém TRUE caso a tecla informada esteja liberada.
-		constexpr bool IsKeyUp(size_t key) const {
+		constexpr bool IsKeyUp(size_t vkkey) const {
 			if (index == 0)
-				return true;
+				return true;			
 
-			if (key > 255)
-				return false;
-
-			for (size_t i = 0; i < MAX_KEYS; ++i) {
-				if (downKeys[i] == key)
-					return false;
-				else if (i == -1)
-					return true;
-			}
-
-			return true;
+			return !checkSlots((int)vkkey);
 		}
 
 		//Operador para acesso direto para verificar se uma tecla está pressionada
@@ -79,33 +49,126 @@ namespace dxna::input {
 		}
 
 		//Operador para acesso direto para verificar se uma tecla está pressionada
-		constexpr KeyState operator[](size_t const& key) const {
-			return IsKeyDown(key) ? KeyState::Down : KeyState::Up;
+		constexpr KeyState operator[](size_t const& vkkey) const {
+			return IsKeyDown(vkkey) ? KeyState::Down : KeyState::Up;
 		}
 
-		//Adiciona teclas pressionadas ao estado atual, com o máximo de 20 registros.
-		constexpr void AddPressedKey(Keys const& key) {
-			downKeys[index++] = (int)key;
-			
-			if (index == MAX_KEYS)
-				index = 0;
-		}
-
-		//Adiciona teclas pressionadas ao estado atual, com o máximo de 20 registros.
-		constexpr void AddPressedKey(size_t const& key) {
-			downKeys[index++] = static_cast<int>(key);
+		//Internal: Adiciona teclas pressionadas ao estado atual, com o máximo de 10 registros.
+		//Retorna true caso a operação seja bem sucedida ou não exceda os 10 registros.
+		constexpr bool AddPressedKey(Keys const& key) {
+			++index;
 
 			if (index == MAX_KEYS)
-				index = 0;
+				return false;
+
+			switch (index)
+			{
+			case 0:
+				slot0 = static_cast<int>(key);
+				break;
+			case 1:
+				slot1 = static_cast<int>(key);
+				break;
+			case 2:
+				slot2 = static_cast<int>(key);
+				break;
+			case 3:
+				slot3 = static_cast<int>(key);
+				break;
+			case 4:
+				slot4 = static_cast<int>(key);
+				break;
+			case 5:
+				slot5 = static_cast<int>(key);
+				break;
+			case 6:
+				slot6 = static_cast<int>(key);
+				break;
+			case 7:
+				slot7 = static_cast<int>(key);
+				break;
+			case 8:
+				slot8 = static_cast<int>(key);
+				break;
+			case 9:
+				slot9 = static_cast<int>(key);
+				break;
+			default:
+				return false;
+			}
+
+			return true;
 		}
+
+		//Internal: Adiciona teclas pressionadas ao estado atual, com o máximo de 10 registros.
+		//Retorna true caso a operação seja bem sucedida ou não exceda os 10 registros.
+		constexpr bool AddPressedKey(size_t const& vkkey) {
+			++index;
+
+			if (index == MAX_KEYS)
+				return false;
+
+			switch (index)
+			{
+			case 0:
+				slot0 = static_cast<int>(vkkey);
+				break;
+			case 1:
+				slot1 = static_cast<int>(vkkey);
+				break;
+			case 2:
+				slot2 = static_cast<int>(vkkey);
+				break;
+			case 3:
+				slot3 = static_cast<int>(vkkey);
+				break;
+			case 4:
+				slot4 = static_cast<int>(vkkey);
+				break;
+			case 5:
+				slot5 = static_cast<int>(vkkey);
+				break;
+			case 6:
+				slot6 = static_cast<int>(vkkey);
+				break;
+			case 7:
+				slot7 = static_cast<int>(vkkey);
+				break;
+			case 8:
+				slot8 = static_cast<int>(vkkey);
+				break;
+			case 9:
+				slot9 = static_cast<int>(vkkey);
+				break;
+			default:
+				return false;
+			}
+
+			return true;
+		}		
 
 	private:		
-		static constexpr int MAX_KEYS = 10;
-		size_t index{ 0 };
+		constexpr bool checkSlots(int const& value) const {
+			return value == slot0 || value == slot1
+				|| value == slot2 || value == slot3
+				|| value == slot4 || value == slot5
+				|| value == slot6 || value == slot7
+				|| value == slot8 || value == slot9;
+		}
 
-		//Vetor de teclas pressionadas com valores inicializados em -1, onde as teclas pressionadas são números inteiros positivos.
-		//Na consulta, se o index do vetor tiver o valor -1 significa que não há mais teclas pressionadas a serem verificadas.
-		std::vector<int> downKeys = std::vector<int>(MAX_KEYS, -1);		
+		static constexpr int MAX_KEYS = 10;
+		
+		int index{ -1 };		
+		int slot0{ -1 };
+		int slot1{ -1 };
+		int slot2{ -1 };
+		int slot3{ -1 };
+		int slot4{ -1 };
+		int slot5{ -1 };
+		int slot6{ -1 };
+		int slot7{ -1 };
+		int slot8{ -1 };
+		int slot9{ -1 };
 	};
 
 	struct Keyboard {
@@ -126,11 +189,11 @@ namespace dxna::input {
 		}
 
 		//Obtém TRUE caso a tecla informada esteja pressionada.
-		bool constexpr IsKeyDown(size_t key) const {
-			if (key > 255)
+		bool constexpr IsKeyDown(size_t vkkey) const {
+			if (vkkey > 255)
 				return false;
 
-			return flags[key];
+			return flags[vkkey];
 		}
 
 		//Obtém TRUE caso a tecla informada esteja liberada.
@@ -139,18 +202,18 @@ namespace dxna::input {
 		}
 
 		//Obtém TRUE caso a tecla informada esteja liberada.
-		bool constexpr IsKeyUp(size_t key) const {
-			if (key > 255)
+		bool constexpr IsKeyUp(size_t vkkey) const {
+			if (vkkey > 255)
 				return false;
 
-			return !flags[(int)key];
+			return !flags[(int)vkkey];
 		}
 
-		static constexpr void SetFlag(size_t index, bool value) {
-			if (index > 255)
+		static constexpr void SetFlag(size_t vkkey, bool value) {
+			if (vkkey > 255)
 				return;
 
-			flags[index] = value;
+			flags[vkkey] = value;
 		}
 
 	private:
